9862430bf6745bc59f585503e024c9ab8add961c
Offical-Dhiraj <dhirajkrraj824113@gmail.com>
2025-12-19 12:05:36 +0530

    modified in app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,14 +6,19 @@ import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
+ import { ToastContainer} from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl=import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
   const [token, setToken] = useState("");
 
   return (
     <div className="bg-gray-50 min-h-screen">
+      <ToastContainer/>
       {token === "" ? (
-        <Login />
+        <Login setToken={setToken} />
       ) : 
         <>
           <Navbar />
