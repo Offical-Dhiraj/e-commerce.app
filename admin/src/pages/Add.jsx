5634b3e31475d117bbbb72d5aa203a19e6bb5f3f
Offical-Dhiraj <dhirajkrraj824113@gmail.com>
2025-12-21 18:14:21 +0530

    modified in admin add.jsx page

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import axios from 'axios'
+import { backendUrl } from "../App";
 
-const Add = () => {
+const Add = ({token}) => {
   const [image1, setImage1] = useState(false);
   const [image2, setImage2] = useState(false);
   const [image3, setImage3] = useState(false);
@@ -11,12 +13,44 @@ const Add = () => {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("Men");
-  const [SubCategory, setSubCategory] = useState("Topwear");
+  const [subCategory, setSubCategory] = useState("Topwear");
   const [bestseller, setBestseller] = useState(false);
   const [sizes, setSizes] = useState([]);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData=new FormData()
+
+      formData.append("name",name)
+      formData.append("description",description)
+      formData.append("price",price)
+      formData.append("category",category)
+      formData.append("subCategory",subCategory)
+      formData.append("sizes",JSON.stringify(sizes))
+      formData.append("bestseller",bestseller)
+      
+      image1 && formData.append("image1",image1)
+      image2 && formData.append("image2",image2)
+      image3 && formData.append("image3",image3)
+      image4 && formData.append("image4",image4)
+      
+
+      const response=await axios.post(backendUrl + "/api/product/add",formData,{headers:{token}})
+      console.log(response.data);
+      
+
+    } catch (error) {
+      
+    }
+  };
+
   return (
-    <form className="flex flex-col w-full items-start gap-3">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col w-full items-start gap-3"
+    >
       <div>
         <p className="mb-2">Upload Image</p>
         <div className="flex gap-2">
@@ -231,7 +265,12 @@ const Add = () => {
       </div>
 
       <div className="flex gap-2 mt-2">
-        <input type="checkbox" id="bestseller" />
+        <input
+          onChange={() => setBestseller((prev) => !prev)}
+          checked={bestseller}
+          type="checkbox"
+          id="bestseller"
+        />
         <label className="cursor-pointer" htmlFor="bestseller">
           Add to bestseller
         </label>
